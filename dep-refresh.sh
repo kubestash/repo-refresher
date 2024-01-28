28d4fed55de116505e3d0330fddfd1ef13efec1f
Tamal Saha <tamal@appscode.com>
2024-01-28 02:24:13 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- dep-refresh.sh
+++ dep-refresh.sh
@@ -5,12 +5,12 @@ SCRIPT_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
 
 GITHUB_USER=${GITHUB_USER:-1gtm}
-PR_BRANCH=k129-auto # -$(date +%s)
-COMMIT_MSG="Use k8s 1.29 client libs"
+PR_BRANCH=ctrl2 # -$(date +%s)
+COMMIT_MSG="Use deps"
 
 REPO_ROOT=/tmp/kubedb-repo-refresher
 
-API_REF=${API_REF:-cc46ddfd674a760d87ec2fe4122f7816296654c8}
+API_REF=${API_REF:-91fea71b}
 
 repo_uptodate() {
     # gomodfiles=(go.mod go.sum vendor/modules.txt)
@@ -61,11 +61,14 @@ refresh() {
             -require=go.bytebuilders.dev/audit@3ff33160c6f02f6151e59cdd44dd50a347c02ba0 \
             -require=github.com/elastic/go-elasticsearch/v7@v7.15.1 \
             -require=go.mongodb.org/mongo-driver@v1.10.2 \
+            -require=sigs.k8s.io/controller-runtime@v0.17.0 \
             -replace=github.com/Masterminds/sprig/v3=github.com/gomodules/sprig/v3@v3.2.3-0.20220405051441-0a8a99bac1b8 \
             -replace=sigs.k8s.io/controller-runtime=github.com/kmodules/controller-runtime@ac-0.17.0 \
             -replace=github.com/imdario/mergo=github.com/imdario/mergo@v0.3.6 \
             -replace=k8s.io/apiserver=github.com/kmodules/apiserver@ac-1.29.0 \
-            -replace=k8s.io/kubernetes=github.com/kmodules/kubernetes@ac-1.29.0
+            -replace=k8s.io/kubernetes=github.com/kmodules/kubernetes@ac-1.29.0 \
+            -require=github.com/docker/docker@v24.0.7+incompatible \
+            -require=github.com/docker/cli@v24.0.7+incompatible
 
         # sed -i 's|NewLicenseEnforcer|MustLicenseEnforcer|g' `grep 'NewLicenseEnforcer' -rl *`
         go mod tidy
