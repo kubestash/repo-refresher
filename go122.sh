d69dec9d7f4077876288ed8c758d2af74e3cd7d3
Tamal Saha <tamal@appscode.com>
2024-06-04 11:32:27 -0700

    wip
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- go122.sh
+++ go122.sh
@@ -5,10 +5,10 @@ SCRIPT_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
 
 GITHUB_USER=${GITHUB_USER:-1gtm}
-PR_BRANCH=go122 # -$(date +%s)
-COMMIT_MSG="Use Go 1.22"
+PR_BRANCH=k130 # -$(date +%s)
+COMMIT_MSG="Use k8s 1.30 client libs"
 
-REPO_ROOT=/tmp/stash-repo-refresher
+REPO_ROOT=/tmp/kubedb-repo-refresher
 
 API_REF=${API_REF:-c5efabadb}
 
@@ -60,26 +60,25 @@ refresh() {
 
     if [ -f go.mod ]; then
         go mod edit \
-            -require=kmodules.xyz/client-go@v0.29.13 \
-            -require=kmodules.xyz/resource-metadata@v0.18.2 \
+            -require=kubestash.dev/apimachinery@126c3590 \
+            -require=kmodules.xyz/client-go@v0.30.1 \
+            -require=kmodules.xyz/resource-metadata@v0.18.6 \
             -require=gomodules.xyz/password-generator@v0.2.9 \
-            -require=go.bytebuilders.dev/license-verifier@v0.14.0 \
-            -require=go.bytebuilders.dev/license-verifier/kubernetes@v0.14.0 \
-            -require=go.bytebuilders.dev/license-proxyserver@v0.0.9 \
-            -require=go.bytebuilders.dev/audit@v0.0.33 \
+            -require=go.bytebuilders.dev/license-verifier@v0.14.1 \
+            -require=go.bytebuilders.dev/license-verifier/kubernetes@v0.14.1 \
+            -require=go.bytebuilders.dev/license-proxyserver@v0.0.10 \
+            -require=go.bytebuilders.dev/audit@v0.0.35 \
             -require=golang.org/x/net@v0.23.0 \
             -require=github.com/golang/protobuf@v1.5.4 \
             -require=google.golang.org/protobuf@v1.33.0 \
             -require=github.com/docker/docker@v24.0.9+incompatible \
-            -require=github.com/docker/cli@v24.0.9+incompatible
-
-            # -replace=github.com/Masterminds/sprig/v3=github.com/gomodules/sprig/v3@v3.2.3-0.20220405051441-0a8a99bac1b8 \
-            # -replace=sigs.k8s.io/controller-runtime=github.com/kmodules/controller-runtime@ac-0.17.0 \
-            # -replace=github.com/imdario/mergo=github.com/imdario/mergo@v0.3.6 \
-            # -replace=k8s.io/apiserver=github.com/kmodules/apiserver@ac-1.29.0 \
-            # -replace=k8s.io/kubernetes=github.com/kmodules/kubernetes@ac-1.29.0 \
-            # -require=github.com/docker/docker@v24.0.9+incompatible \
-            # -require=github.com/docker/cli@v24.0.9+incompatible
+            -require=github.com/docker/cli@v24.0.9+incompatible \
+            -require=kubevault.dev/apimachinery@v0.18.2 \
+            -replace=github.com/Masterminds/sprig/v3=github.com/gomodules/sprig/v3@v3.2.3-0.20220405051441-0a8a99bac1b8 \
+            -replace=sigs.k8s.io/controller-runtime=github.com/kmodules/controller-runtime@ac-0.18.3 \
+            -replace=github.com/imdario/mergo=github.com/imdario/mergo@v0.3.6 \
+            -replace=k8s.io/apiserver=github.com/kmodules/apiserver@ac-1.30.1 \
+            -dropreplace=k8s.io/kubernetes
 
         # sed -i 's|NewLicenseEnforcer|MustLicenseEnforcer|g' `grep 'NewLicenseEnforcer' -rl *`
         go mod tidy
